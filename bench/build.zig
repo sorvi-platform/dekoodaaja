12f1a2f8bedb59f83db1fa09593c90cfc968d091
Jari Vetoniemi <jari.vetoniemi@cloudef.pw>
2026-08-29 14:24:21 +0900

    bench: make it compile on aarch64

--- bench/build.zig
+++ bench/build.zig
@@ -12,99 +12,6 @@ pub fn build(b: *std.Build) void {
     const external = b.option(bool, "external", "include external decoders in benchmark") orelse true;
 
     const dekoodaaja = b.dependency("dekoodaaja", .{ .target = target, .optimize = optimize }).module("dekoodaaja");
-    const zig_qoi = b.dependency("zig-qoi", .{ .target = target, .optimize = optimize }).module("qoi");
-    zig_qoi.strip = false;
-    zig_qoi.link_libc = false;
-
-    const zqoi = b.dependency("zqoi", .{ .target = target, .optimize = optimize }).module("root");
-    zqoi.strip = false;
-    zqoi.link_libc = false;
-
-    const qoi = D: {
-        const dep = b.dependency("qoi", .{});
-        const c = b.addTranslateC(.{
-            .root_source_file = dep.path("qoi.h"),
-            .optimize = optimize,
-            .target = target,
-            .link_libc = true,
-        });
-        const mod = c.createModule();
-        const files = b.addWriteFiles();
-        _ = files.add("qoi.c",
-            \\#define QOI
-            \\#define QOI_IMPLEMENTATION
-            \\#define QOI_MALLOC(sz) dekoodaaja_bench_malloc(sz)
-            \\#define QOI_FREE(p) ((void)p)
-            \\#include <stddef.h>
-            \\void* dekoodaaja_bench_malloc(size_t sz);
-            \\#include "qoi.h"
-        );
-        mod.addCSourceFiles(.{
-            .files = &.{"qoi.c"},
-            .root = files.getDirectory(),
-        });
-        mod.addIncludePath(dep.path(""));
-        mod.strip = false;
-        break :D mod;
-    };
-
-    const qoi_simd = D: {
-        const dep = b.dependency("qoi-simd", .{});
-        const c = b.addTranslateC(.{
-            .root_source_file = b.addWriteFiles().add("c.h",
-                \\#define qoi_encode simd_qoi_encode
-                \\#define qoi_decode simd_qoi_decode
-                \\#define qoi_write simd_qoi_write
-                \\#define qoi_read simd_qoi_read
-                \\#include "qoi.h"
-            ),
-            .optimize = optimize,
-            .target = target,
-            .link_libc = true,
-        });
-        c.addIncludePath(dep.path(""));
-        const mod = c.createModule();
-        const files = b.addWriteFiles();
-        _ = files.add("qoi.c",
-            \\#define QOI
-            \\#define QOI_IMPLEMENTATION
-            \\#define QOI_MALLOC(sz) dekoodaaja_bench_malloc(sz)
-            \\#define QOI_FREE(p) ((void)p)
-            \\#include <stddef.h>
-            \\void* dekoodaaja_bench_malloc(size_t sz);
-            \\#define qoi_encode simd_qoi_encode
-            \\#define qoi_decode simd_qoi_decode
-            \\#define qoi_write simd_qoi_write
-            \\#define qoi_read simd_qoi_read
-            \\#include "qoi.h"
-        );
-        mod.addCSourceFiles(.{
-            .files = &.{"qoi.c"},
-            .root = files.getDirectory(),
-        });
-        mod.addIncludePath(dep.path(""));
-        mod.strip = false;
-        break :D mod;
-    };
-
-    const magicqoi = D: {
-        const dep = b.dependency("magicqoi", .{});
-        const c = b.addTranslateC(.{
-            .root_source_file = dep.path("magicqoi/magicqoi.h"),
-            .optimize = optimize,
-            .target = target,
-            .link_libc = true,
-        });
-        c.addIncludePath(dep.path(""));
-        const mod = c.createModule();
-        mod.addCSourceFiles(.{
-            .files = &.{"magicqoi.c"},
-            .root = dep.path("magicqoi"),
-        });
-        mod.addIncludePath(dep.path(""));
-        mod.strip = false;
-        break :D mod;
-    };
 
     const include_rust = D: {
         if (!external) break :D false;
@@ -127,15 +34,121 @@ pub fn build(b: *std.Build) void {
             .imports = &.{
                 .{ .name = "build_options", .module = opts.createModule() },
                 .{ .name = "dekoodaaja", .module = dekoodaaja },
-                .{ .name = "qoi", .module = qoi },
-                .{ .name = "qoi-simd", .module = qoi_simd },
-                .{ .name = "magicqoi", .module = magicqoi },
-                .{ .name = "zig-qoi", .module = zig_qoi },
-                .{ .name = "zqoi", .module = zqoi },
             },
         }),
     });
-    bench.lto = .full;
+
+    bench.lto = switch (target.result.ofmt) {
+        .macho => .none,
+        else => .full,
+    };
+
+    if (external) {
+        const zig_qoi = b.dependency("zig-qoi", .{ .target = target, .optimize = optimize }).module("qoi");
+        zig_qoi.strip = false;
+        zig_qoi.link_libc = false;
+
+        const zqoi = b.dependency("zqoi", .{ .target = target, .optimize = optimize }).module("root");
+        zqoi.strip = false;
+        zqoi.link_libc = false;
+
+        const qoi = D: {
+            const dep = b.dependency("qoi", .{});
+            const c = b.addTranslateC(.{
+                .root_source_file = dep.path("qoi.h"),
+                .optimize = optimize,
+                .target = target,
+                .link_libc = true,
+            });
+            const mod = c.createModule();
+            const files = b.addWriteFiles();
+            _ = files.add("qoi.c",
+                \\#define QOI
+                \\#define QOI_IMPLEMENTATION
+                \\#define QOI_MALLOC(sz) dekoodaaja_bench_malloc(sz)
+                \\#define QOI_FREE(p) ((void)p)
+                \\#include <stddef.h>
+                \\void* dekoodaaja_bench_malloc(size_t sz);
+                \\#include "qoi.h"
+            );
+            mod.addCSourceFiles(.{
+                .files = &.{"qoi.c"},
+                .root = files.getDirectory(),
+            });
+            mod.addIncludePath(dep.path(""));
+            mod.strip = false;
+            break :D mod;
+        };
+
+        const qoi_simd = D: {
+            const dep = b.dependency("qoi-simd", .{});
+            const c = b.addTranslateC(.{
+                .root_source_file = b.addWriteFiles().add("c.h",
+                    \\#define qoi_encode simd_qoi_encode
+                    \\#define qoi_decode simd_qoi_decode
+                    \\#define qoi_write simd_qoi_write
+                    \\#define qoi_read simd_qoi_read
+                    \\#include "qoi.h"
+                ),
+                .optimize = optimize,
+                .target = target,
+                .link_libc = true,
+            });
+            c.addIncludePath(dep.path(""));
+            const mod = c.createModule();
+            const files = b.addWriteFiles();
+            _ = files.add("qoi.c",
+                \\#define QOI
+                \\#define QOI_IMPLEMENTATION
+                \\#define QOI_MALLOC(sz) dekoodaaja_bench_malloc(sz)
+                \\#define QOI_FREE(p) ((void)p)
+                \\#include <stddef.h>
+                \\void* dekoodaaja_bench_malloc(size_t sz);
+                \\#define qoi_encode simd_qoi_encode
+                \\#define qoi_decode simd_qoi_decode
+                \\#define qoi_write simd_qoi_write
+                \\#define qoi_read simd_qoi_read
+                \\#include "qoi.h"
+            );
+            mod.addCSourceFiles(.{
+                .files = &.{"qoi.c"},
+                .root = files.getDirectory(),
+            });
+            mod.addIncludePath(dep.path(""));
+            mod.strip = false;
+            break :D mod;
+        };
+
+        const magicqoi = D: {
+            const dep = b.dependency("magicqoi", .{});
+            const c = b.addTranslateC(.{
+                .root_source_file = dep.path("magicqoi/magicqoi.h"),
+                .optimize = optimize,
+                .target = target,
+                .link_libc = true,
+            });
+            c.addIncludePath(dep.path(""));
+            const mod = c.createModule();
+            mod.addCSourceFiles(.{
+                .files = &.{"magicqoi.c"},
+                .root = dep.path("magicqoi"),
+            });
+            mod.addIncludePath(dep.path(""));
+            mod.strip = false;
+            break :D mod;
+        };
+
+        switch (target.result.cpu.arch) {
+            .x86_64 => {
+                bench.root_module.addImport("qoi-simd", qoi_simd);
+            },
+            else => {},
+        }
+        bench.root_module.addImport("qoi", qoi);
+        bench.root_module.addImport("magicqoi", magicqoi);
+        bench.root_module.addImport("zig-qoi", zig_qoi);
+        bench.root_module.addImport("zqoi", zqoi);
+    }
 
     if (include_rust) {
         const rs = D: {
